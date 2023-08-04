405b986ec3ef48f0e193e94ab97432de793443cd
Danilo Pianini <danilo.pianini@unibo.it>
2023-08-04 13:24:41 +0200

    fix(ci): release a latest version on dockerhub

--- release.config.js
+++ release.config.js
@@ -2,8 +2,8 @@ var publishCmd = `
 echo "$DOCKER_PASSWORD" | docker login -u danysk --password-stdin
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
 git push --force origin \${nextRelease.version} || exit 2
-docker build -t danysk/acsos23-telegram-bot:\${nextRelease.version} . || exit 3
-docker push danysk/acsos23-telegram-bot:\${nextRelease.version} || exit 4
+docker build -t danysk/acsos23-telegram-bot:\${nextRelease.version} -t danysk/acsos23-telegram-bot:latest . || exit 3
+docker push --all-tags danysk/acsos23-telegram-bot:\${nextRelease.version} || exit 4
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
