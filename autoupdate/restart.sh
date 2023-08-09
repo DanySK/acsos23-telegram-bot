445cea68f647e3dca5c82e136cbe3195a0f0fe98
Danilo Pianini <danilo.pianini@unibo.it>
2023-08-09 18:28:19 +0200

    fix: try to fix the service create command

--- autoupdate/restart.sh
+++ autoupdate/restart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker service create -e BOT_TOKEN=$BOT_TOKEN danysk/acsos23-telegram-bot:latest acsos23-telegram-bot
+docker service create -e BOT_TOKEN=$BOT_TOKEN --name acsos23-telegram-bot danysk/acsos23-telegram-bot:latest
 while true; do
     docker pull danysk/acsos23-telegram-bot:latest
     docker service update --image danysk/acsos23-telegram-bot:latest acsos23-telegram-bot
