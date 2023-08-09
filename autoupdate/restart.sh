203355c13dee08720db18c4661a1bc348f815da2
Danilo Pianini <danilo.pianini@unibo.it>
2023-08-09 18:05:38 +0200

    fix: fix broken image name in autoupdater

--- autoupdate/restart.sh
+++ autoupdate/restart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-docker service create -e BOT_TOKEN=$BOT_TOKEN danysk/danysk/acsos23-telegram-bot:latest acsos23-telegram-bot
+docker service create -e BOT_TOKEN=$BOT_TOKEN danysk/acsos23-telegram-bot:latest acsos23-telegram-bot
 while true; do
-    docker pull danysk/danysk/acsos23-telegram-bot:latest
-    docker service update --image danysk/danysk/acsos23-telegram-bot:latest acsos23-telegram-bot 
+    docker pull danysk/acsos23-telegram-bot:latest
+    docker service update --image danysk/acsos23-telegram-bot:latest acsos23-telegram-bot
     sleep 1m
 done
