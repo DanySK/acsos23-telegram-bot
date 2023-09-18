8995e0e47613ba8ae04d2efee0053aa9a37bdfbb
Danilo Pianini <danilo.pianini@unibo.it>
2023-09-18 16:40:04 +0200

    feat: try to make the bot tag-sensitive

--- src/main/kotlin/org/danilopianini/acsos23/Bot.kt
+++ src/main/kotlin/org/danilopianini/acsos23/Bot.kt
@@ -4,6 +4,7 @@ package org.danilopianini.acsos23
 
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.newChatMembers
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
 
@@ -16,9 +17,18 @@ fun main() {
             "Please set the BOT_TOKEN environment variable"
         }
         dispatch {
+            newChatMembers {
+                val username = message.from?.apply {
+                    username?.let { "@$it" } ?: "$firstName $lastName"
+                }
+                if (username != null) {
+                    bot.sendMessage(ChatId.fromId(message.chat.id), text = "Welcome to ACSOS 2023, $username!")
+                }
+            }
             text {
-                bot.sendMessage(ChatId.fromId(message.chat.id), text = text)
-                bot.sendMessage(ChatId.fromId(message.chat.id), text = text)
+                if (message.text.orEmpty().contains("@acsos_23_bot")) {
+                    bot.sendMessage(ChatId.fromId(message.chat.id), text = "I have been mentioned!")
+                }
             }
         }
     }
